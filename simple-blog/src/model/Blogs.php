6a36670c77e8e109b4fe3770c57033bcabe20e3f
harshithm <harshithm@unicourt.com>
2022-12-06 18:31:37 +0530

    Contents of workshop 3

---
+++ simple-blog/src/model/Blogs.php
@@ -0,0 +1,51 @@
+<?php
+namespace Blogs;
+
+include_once "../includes/classes.php";
+use Connection\Connection;
+use PDO;
+
+/**
+ * This class contains all the queries which fetchs data from Blogs table
+ */
+class Blogs extends Connection{
+
+        // trigger the parent construct so that the connection gets created
+        public function __construct()
+        {
+            parent::__construct();
+        }
+
+    /**
+     * Method to fetch all the blogs in desc order of last modified to show the latest on top.
+     */
+    public function fetchAllBlogs()
+    {
+        //query
+        $query = "SELECT users.user_id, blogs.blog_id, blogs.image_path, blogs.blog_title, blogs.blog_content, (to_char(blogs.created_on, 'Mon DD, YYYY HH24:MI')) as created_on, CONCAT(users.first_name, ' ', users.last_name) as created_by FROM blogs 
+        LEFT JOIN users on blogs.created_by = users.user_id 
+        ORDER BY blogs.modified_on DESC";
+
+        $blog_data = $this->_connection->query($query)->fetchAll();
+
+        return $blog_data;
+    }
+
+     /**
+     * Method to fetch total number of blogs created by a user based on user_id.
+     */
+    public function fetchAllBlogsByUserID($user_id)
+    {
+        //query
+        $query = "SELECT users.user_id, blogs.blog_id, blogs.image_path, blogs.blog_title, blogs.blog_content, (to_char(blogs.created_on, 'Mon DD, YYYY HH24:MI')) as created_on, CONCAT(users.first_name, ' ', users.last_name) as created_by FROM blogs 
+        LEFT JOIN users on blogs.created_by = users.user_id 
+        WHERE blogs.created_by = $user_id 
+        ORDER BY blogs.modified_on DESC";
+
+
+        $user_blogs = $this->_connection->query($query)->fetchAll();
+
+        return $user_blogs;
+    }
+}
+?>
---
+++ simple-blog/src/view/user-info.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>HTML & CSS Learning Workshop Blog</title>
+		<meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link rel="stylesheet" href="./css/style.css" />
+	</head>
+    <body>
+
+        
+        <?php 
+            // include the classes and fetch blog and user data to display
+            include './header.php';
+            include_once "../includes/classes.php";
+            use Blogs\Controller\BlogsController;
+            use Blogs\Controller\UsersController;
+        
+            // Fetch user data 
+            $usersControllerObj = new UsersController; 
+            $user_data = $usersControllerObj->fetchUserData($_REQUEST['userId']);
+
+            // Fetch blogs by that user.
+            $blogsControllerObj = new BlogsController; 
+            list($blogs_count, $blogs) = $blogsControllerObj->fetchBlogsByUser($_REQUEST['userId']);
+
+        ?>
+
+
+        <div class="container">
+            
+            <article id="<?php echo $user_data['user_id'] ?>">
+                <div class="post-image">
+                    <img src=".<?php echo $user_data['profile_pic'] ?>" alt="<?php echo $user_data['first_name'] ?>" width="200" height="100">
+                    <h2 class="title"><?php echo $user_data['first_name']. " ". $user_data['last_name'] ?></h2>
+                    <p class="userdata"><?php echo "@".$user_data['username'] ?></p>
+                    <p class="userdata"><?php echo $user_data['profession'] ?></p>
+                    <p class="userdata"><?php echo "Joined ".$user_data['created_on'] ?></p>
+                </div>
+            </article>
+            
+            <h2 class="blogs">Blogs <?php echo "(".$blogs_count.")"?></h2>
+            <?php foreach ($blogs as $blog) : ?>
+                <article id="<?php echo $blog['blog_id'] ?>">
+                    <div class="post-image">
+                        <a href="./single-post.php" title="<?php echo $blog['blog_title'] ?>">
+                            <img src=".<?php echo $blog['image_path'] ?>" alt="<?php echo $blog['blog_title'] ?>" width="900" height="500">
+                        </a>
+                    </div>
+                    <div class="post-content">
+                        <div class="post-details">
+                            <h2 class="title"><a href="./single-post.php" rel="bookmark"><?php echo $blog['blog_title'] ?></a></h2>
+                            <p class="post-meta">
+                            <span class="wrap-posted-on">
+                                By <a href="./user-info.php?userId=<?php echo $blog['user_id'] ?>"><?php echo $blog['created_by'] ?></a>
+                                on <time datetime="<?php echo $blog['created_on'] ?>"><?php echo $blog['created_on'] ?></time>
+                            </span>
+                            </p>
+                        </div>
+                        <div class="content">
+                            <p><?php echo $blog['blog_content'] ?></p>
+                        </div>
+                    </div>
+                </article>
+            <?php endforeach; ?>
+        </div>
+
+        <?php include './footer.php' ?>
+
+    </body>
+</html>
