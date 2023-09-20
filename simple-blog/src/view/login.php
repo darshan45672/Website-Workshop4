497e66d88f724da2389ec836b5681e302b419b70
Darshan bhandary <109609462+darshan45672@users.noreply.github.com>
2023-09-20 12:14:06 +0530

    Update login.php

--- simple-blog/src/view/login.php
+++ simple-blog/src/view/login.php
@@ -12,9 +12,38 @@
 
         // paste the error check code here
 
+        $unauthorised = false;
+        if(isset($_REQUEST['failed'])){
+            $unauthorised = true;
+        }
+
+
     ?>
 
     <!-- Paste Login Form content below-->
+
+    <div class="container">
+        <form action="user-info.php" method="post">
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+
+            <button type="submit" name="btnLogin">Login</button>
+
+            <div style="background-color:#f1f1f1">
+                <span class="psw">Forgot <a href="#">password?</a></span>
+            </div>
+
+            <?php if($unauthorised) : ?>
+                <div class="unauthorised">
+                    <p>Incorrect Username or Password</p>
+                </div>
+            <?php endif ?>
+        </form>
+    </div>
+
 
     <?php 
         include './footer.php';
