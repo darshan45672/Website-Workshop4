e0d7cf3565702fb40e5cf6a9ee7fc2c872eb7154
Darshan bhandary <109609462+darshan45672@users.noreply.github.com>
2023-09-20 12:09:11 +0530

    Update UsersController.php

--- simple-blog/src/controller/UsersController.php
+++ simple-blog/src/controller/UsersController.php
@@ -17,5 +17,59 @@ class UsersController{
         return $userData[0];
     }
 
+
+    /**
+     * Method to validate user credentials and redirect on success.
+     */
+    public function validateLogin($username, $password)
+    {
+        // encrypting the password before validation because in database the passwords encypted and stored.
+        $encryptedPassword = hash("sha256", $password);
+
+        $ObjUsers = new Users;
+        $userData = $ObjUsers->fetchByUserNamePassword($username, $encryptedPassword);
+
+        if(!empty($userData)){
+            // set the user session 
+            $_SESSION['userName'] = $userData[0]['username'];
+            $_SESSION['userId'] = $userData[0]['user_id'];
+
+            return $userData[0];
+        }else{
+            // if login fails redirect back to login pafe with failure message.
+            header('Location: login.php?failed=true');
+            exit;
+        }
+    }
+
+
+    /**
+     * Method to validate and call model save for the user data on signup
+     */
+    public function userSignup($userData){
+        // validate if the username is already taken
+        $objUsers = new Users;
+        $userCount = $objUsers->fetchCountByUserName($userData['uname']);
+
+        if($userCount > 0){
+            header("Location: signup.php?failed=true");
+            exit;
+        }
+
+        // encrypt the password before saving
+        $encryptedPassword = hash("sha256", $userData['psw']);
+        $userData['psw'] = $encryptedPassword;
+        $result = $objUsers->save($userData);
+
+        if(!$result){
+            header('Location: signup.php?failed=true');
+            exit;
+        }
+
+        header("Location: login.php");
+        exit;
+    }
+
+
 }
 ?>
